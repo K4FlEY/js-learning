6215cf0cdf42d3cd7db50d75dd5e6f1a7ec1cc0b
Safal Kafle <127853076+K4FlEY@users.noreply.github.com>
2023-11-03 10:14:08 +0000

    conversion

--- basics_01/03_conversionOperation.js
+++ basics_01/03_conversionOperation.js
@@ -3,3 +3,20 @@ let Number = 33
 let stringNumber = String(Number)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2**2);
+
+
+
+let str1 = "hello"
+let str2 = "safal"
+let str3 = str1 + str3
+console.log(str3);
+
+
+console.log("1" + 2);  = 12
+console.log(1 + "2");  = 12
+console.log(2 + 2 + "2"); = 42 
